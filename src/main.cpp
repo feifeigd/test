210105bee4956dd7cc7d7003c8e4ee50eb0d0479
luofei <13632323692@139.com>
2015-06-09 14:47:14 +0800

    改正

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+
 int main(int argc, char* argv[]){
 	cout << "hello test" << endl;
 	cout << "hello test2" << endl;
-	cou t << "hello test3" << endl;
+	cout << "hello test3" << endl;
 	return 0;
 }
