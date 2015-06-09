e57c110f8fc394d9e095b374a41a5a85232ba131
luofei <13632323692@139.com>
2015-06-09 14:29:15 +0800

    搞点错误给Jenkins看看

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@ using std::endl;
 int main(int argc, char* argv[]){
 	cout << "hello test" << endl;
 	cout << "hello test2" << endl;
-	cout << "hello test3" << endl;
+	cou t << "hello test3" << endl;
 	return 0;
 }
